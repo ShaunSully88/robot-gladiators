f86cd383cd3d713523b0cbf56d6b80946552a835
Shaun Sullivan <shaunsully88@gmail.com>
2022-03-01 12:56:44 -0500

    conditions added for player health and attacks

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,49 @@
-var playerName = window.prompt ("What is your robot's name?");
+var playerName = window.prompt ("What is your robot's name? ");
+var playerHealth= 100;
+var playerAttack= 10;
 
-console.log(playerName)
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("This logs a string, good for leaving yourself a message");
+var enemyName= "Roborto ";
+var enemyHealth= 50;
+var enemyAttack= 12;
 
-console.log(10+10);
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators")
 
-console.log("Our robot's name is" + playerName);
+// Subtract the value of 'playerAttack' from the value of 'enemyHealth and use that result to update the value in the 'enemyHealth' variable
+enemyHealth = enemyHealth - playerAttack;
 
-function fight() {
-    window.alert("The fight has begun!");
+// Log a resulting message to the console so we know that it worked
+console.log (
+    playerName + "attacked " + enemyName + ". " + enemyName + "now has " + enemyHealth + "health remaining."
+);
+
+// check enemy's health
+if (enemyHealth <= 0) {
+    window.alert (enemyName + " has died!");
 }
+else {
+    window.alert (enemyName + " still has" + enemyHealth + " health left.");
+}
+
+// Subtract the value of the 'enemyAttack' from the value of the 'playerHealth' and use that result to update the value in the 'playerHealth' variable
+playerHealth= playerHealth - enemyAttack;
+
+//Log a resulting message to the console so we know that it worked
+console.log(
+    enemyName + "attacked " + playerName + "." + playerName + "now has " + playerHealth + "health remaining."
+);
+
+if (playerHealth <=0) {
+    window.alert (playerName + " has died!");
+}
+else {
+    window.alert (playerName + " still has " + playerHealth + " health left.");
+}
+
+};
 
 fight();
 
-// fight ()
