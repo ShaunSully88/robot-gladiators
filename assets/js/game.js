2aa925e53466ff3ca0835a04fa81e0843c436467
Shaun Sullivan <shaunsully88@gmail.com>
2022-03-04 11:50:34 -0500

    confirm options in place

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,24 +1,32 @@
 
+var fightOrSkip = function () {
+  var promptFight= window.prompt('Would you like to fight or skip this battle? Enter "FIGHT" or "SKIP" to choose.');
 
+  //conditional recursive function here
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer. Try again.");
+    return fightOrSkip();
+  }
+    promptFight= promptFight.toLowerCase();
+  if (promptFight === "SKIP" || promptFight=== "skip") {
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    if (confirmSkip) {
+      window.alert(playerInfo.name + "has decided to skip this fight. Goodbye!");
+      playerInfo.playerMoney = playerInfo.money - 10;
+      shop();
+      return true;
+    }
+  }
+  return false;
+}
 // fight function
 var fight = function(enemy) {
   
   while(playerInfo.health > 0 && enemy.health > 0) {
         
-    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-
-    if (promptFight === "skip" || promptFight === "SKIP") {
-        // confirm player wants to skip
-      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-    
-        // if yes (true), leave fight
-      if (confirmSkip) {
-        window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-          // subtract money from playerMoney for skipping
-        playerInfo.money = Math.max (0,playerInfo.money - 10);
-        console.log("playerinfo.money", playerInfo.money);
-        break;  
-      }
+    if (fightOrSkip()) {
+      break;
     }
           // remove enemy's health by subtracting the amount set in the playerAttack variable
     var damage = randomNumber(playerInfo.attack -3, playerInfo.attack);
